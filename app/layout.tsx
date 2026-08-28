3bbf54ab40e835a58c44b42ea4e8594936c34b57
LinconlBR <linconl1997@gmail.com>
2026-08-28 11:45:10 +0200

    Pages and components
    - Implement Home , navbar, footer , signup and login pages
    -import badge, card, chart, field, input, label, separator components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
 const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
@@ -26,7 +29,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+      
     </html>
   );
 }
---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+
+import EmailPasswordDemo from  "./EmailPasswordDemo"
+
+
+export default function Signup() {
+    return <EmailPasswordDemo user={null} />
+
+}
